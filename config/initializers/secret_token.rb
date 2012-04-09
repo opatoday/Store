b45a6a2e30c7a456153e03e1834690cf22556bd1
Jason Wong <jwong@opatoday.com>
2012-04-09 12:35:22 -0500

    Initial setup with spree plus modifications to checkout form and logo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '1dd5ed926f9d47da2ca3d9ec7bd220b9b5bef4611c7e9ce90c834b770c3c61d05877d9f563e662fe8136d4d7469d92603b11a445f9199733066b416d08f1bde0'
